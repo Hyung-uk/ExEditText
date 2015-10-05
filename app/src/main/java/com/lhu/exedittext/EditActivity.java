cdb690d64f07b6303eccbf52a7ce44c8534023e2
hyung-uk <lhu114@nate.com>
2015-10-05 10:31:50 +0900

    nextFocus, imeOptions, addTextChangedListener

--- app/src/main/java/com/lhu/exedittext/EditActivity.java
+++ app/src/main/java/com/lhu/exedittext/EditActivity.java
@@ -1,16 +1,60 @@
 package com.lhu.exedittext;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class EditActivity extends AppCompatActivity {
+    EditText pswdEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+
+        pswdEdit = (EditText) findViewById(R.id.editText2);
+        pswdEdit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String text = s.toString();
+
+                if(text.length() < 6) {
+                    pswdEdit.setTextColor(Color.RED);
+                } else {
+                    pswdEdit.setTextColor(Color.BLACK);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        pswdEdit.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEND) {
+                    Toast.makeText(EditActivity.this, "Clicked", Toast.LENGTH_SHORT).show();
+                }
+
+                return false;
+            }
+        });
     }
 
     @Override
